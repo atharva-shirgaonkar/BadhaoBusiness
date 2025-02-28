a57f500379ae2dc0baf1a129b2b75a380d1c4f85
Atharva Shirgaonkar <154105625+atharva-shirgaonkar@users.noreply.github.com>
2025-02-28 15:18:44 +0530

    Updated
    Logo and several things

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * toggle navbar
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+/**video */
+
+
+document.getElementById("playButton").addEventListener("click", function () {
+  const aboutBanner = document.getElementById("aboutBanner");
+  const aboutVideo = document.getElementById("aboutVideo");
+
+  // Hide the image and play button
+  aboutBanner.classList.add("hidden");
+
+  // Show the video
+  aboutVideo.classList.remove("hidden");
+  aboutVideo.classList.add("visible");
+
+  // Play the video
+  aboutVideo.play();
+});
+
